3c88e6ec12bc3a1e00510e94594a239cbf3d12a6
HerrisGithub <131113514@students.mikroskil.ac.id>
2015-06-10 19:25:40 +0700

    Edit Theme Bespoke

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
-  cube = require('bespoke-theme-cube'),
+  cube = require('bespoke-theme-fancy'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
